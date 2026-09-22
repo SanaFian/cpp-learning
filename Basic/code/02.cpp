acd8572c81d298028d13f43ee8d06001a2457c80
Sana Najafian <sanamer.fian@gmail.com>
2026-09-22 17:46:19 +0330

    Update 02.cpp

--- Basic/code/02.cpp
+++ Basic/code/02.cpp
@@ -5,11 +5,10 @@ int main()
 {
     cout << "\n\n Print the following pattern:\n";
 	cout << "--------------------------------\n";
-	cout << " ++++++\n";
-	cout << "+      +       +        +\n";
-	cout << "+              +        +\n";
-	cout << "+          +++++++  +++++++\n";
-	cout << "+              +        +\n";
-	cout << "+      +       +        +\n";
-	cout << " ++++++\n";
+	cout << "  +\n";
+	cout << " +++\n";
+	cout << "+++++\n";
+	cout << " +++\n";
+	cout << "  +\n";
+
 }
